f79bee3cc3b30ae69c18a8125d7a3511a0254826
aonnapaht <napaht_d@cmu.ac.th>
2023-03-03 15:19:27 +0700

    napaht

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }
+    else {
+    cout << "---------------------------------\n";
+    double sum = 0;
+    for(int i = 0;i < argc ;i++){
+        sum += atoi(argv[i]);
+    }
+    cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << '\n';
+    cout << "---------------------------------\n";
+    }
+
+}
